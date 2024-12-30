a0333a15037774ade06cc51c96e6e450d8f562ca
fiqrioemry <foemry@gmail.com>
2024-12-30 22:27:41 +0700

    updates changes

--- client/src/components/post/Posts.jsx
+++ client/src/components/post/Posts.jsx
@@ -10,10 +10,8 @@ import { initialCommentConfig, initialCommentForm } from "../../config";
 const Posts = ({ posts, message, recommend }) => {
   const { handleOpenModal } = useProvider();
 
-  console.log(recommend);
-
   return (
-    <div className="w-full max-w-[30em] py-12 space-y-3">
+    <div>
       {posts.length === 0 ? (
         <div className="h-60 w-full flex flex-col items-center justify-center">
           <h1 className="text-2xl font-semibold">{message}</h1>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,6 +3,8 @@ import Posts from "../components/post/Posts";
 import { usePostStore } from "../store/usePostStore";
 import DetailPostModal from "../components/modal/DetailPostModal";
 import { useFollowStore } from "../store/useFollowStore";
+import PostsLoadingSkeleton from "../components/skeleton/PostsLoadingSkeleton";
+import { Button } from "../components/ui/button";
 
 const Home = () => {
   const { getFollowRecommendations, recommendUsers } = useFollowStore();
@@ -15,13 +17,11 @@ const Home = () => {
 
   return (
     <div className="flex">
-      <div className="flex-grow">
+      <div className="flex-grow flex justify-center">
         <DetailPostModal />
-        <div className="flex justify-center">
-          {!followingPosts && !recommendUsers ? (
-            <div className="h-screen flex items-center justify-center">
-              Loading ...
-            </div>
+        <div className="w-full max-w-[30em] mt-12 mb-12 md:mt-6 md:mb-6  space-y-6 py-6 md:py-0">
+          {recommendUsers ? (
+            <PostsLoadingSkeleton />
           ) : (
             <Posts
               message={message}
@@ -31,7 +31,43 @@ const Home = () => {
           )}
         </div>
       </div>
-      <div className="w-[24rem] xl:block hidden"></div>
+      <div className="w-[22rem] xl:flex justify-center hidden ">
+        <div className="px-12 space-y-4 bg-red-500 ">
+          <div className="w-full p-2 border">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center space-x-4">
+                <div className="h-14 w-14 rounded-full bg-red-500"></div>
+                <div className="text-sm">
+                  <div className="font-semibold">arya.ketaren</div>
+                  <div className="font-normal">Suggested for you</div>
+                </div>
+              </div>
+              <div>
+                <Button variant="custom" size="md">
+                  Follow
+                </Button>
+              </div>
+            </div>
+          </div>
+
+          <div className="w-96 p-2 border">
+            <div className="flex items-center justify-between">
+              <div className="flex items-center space-x-4">
+                <div className="h-14 w-14 rounded-full bg-red-500"></div>
+                <div className="text-sm">
+                  <div className="font-semibold">arya.ketaren</div>
+                  <div className="font-normal">Suggested for you</div>
+                </div>
+              </div>
+              <div>
+                <Button variant="custom" size="md">
+                  Follow
+                </Button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
