971553b64086d2fdcaedcbf8a53e1db2eb0e87b9
fiqrioemry <foemry@gmail.com>
2025-03-01 22:12:51 +0700

    update changes

--- client/src/components/layout/backup.jsx
+++ client/src/components/layout/backup.jsx
@@ -7,16 +7,17 @@ import { ScrollArea } from "@/components/ui/scroll-area";
 
 const Layout = () => {
   return (
-    <Fragment className="h-screen">
+    <Fragment>
       <Navbar />
-
-      <ScrollArea className="flex overflow-y-auto ">
+      <main className="flex">
         <SideNavbar />
-        <div className="flex-1">
-          <Outlet />
-        </div>
-      </ScrollArea>
-      <BottomNavbar />
+        <ScrollArea className="flex-1 h-screen overflow-y-auto ">
+          <div className="flex justify-center h-screen mt-20 mb-20 md:mt-0 md:mb-0">
+            <Outlet />
+          </div>
+        </ScrollArea>
+        <BottomNavbar />
+      </main>
     </Fragment>
   );
 };
--- client/src/pages/Testing.jsx
+++ client/src/pages/Testing.jsx
@@ -1,11 +1,57 @@
 const Testing = () => {
   return (
-    <div>
-      <header className="fixed top-0 left-0 right-0 bg-blue-500 py-10 text-center">
+    <div className="h-screen flex flex-col">
+      <header className="block md:hidden h-14 top-0 left-0 right-0 bg-blue-500 text-center">
         <h1>header</h1>
       </header>
-      <div className="h-screen overflow-y-scroll"></div>
-      <footer className="fixed bottom-0 left-0 right-0 bg-red-500 py-10 text-center">
+      <div className="flex-1 flex">
+        {/* sidebar */}
+        <div className="hidden md:block bg-green-500 top-0 w-72 bottom-0 left-0 z-10"></div>
+
+        <div className="flex-1 h-[81vh] md:h-screen overflow-y-auto">
+          <div className="mx-2 md:mx-8 py-4 space-y-2 ">
+            <div className="h-14 bg-yellow-200 text-center">PERTAMA</div>
+            <div className="h-14 bg-yellow-200 text-center">
+              content halaman
+            </div>
+            <div className="h-14 bg-yellow-200 text-center">
+              content halaman
+            </div>
+            <div className="h-14 bg-yellow-200 text-center">
+              content halaman
+            </div>
+            <div className="h-14 bg-yellow-200 text-center">
+              content halaman
+            </div>
+            <div className="h-14 bg-yellow-200 text-center">
+              content halaman
+            </div>
+            <div className="h-14 bg-yellow-200 text-center">
+              content halaman
+            </div>
+            <div className="h-14 bg-yellow-200 text-center">
+              content halaman
+            </div>
+            <div className="h-14 bg-yellow-200 text-center">
+              content halaman
+            </div>
+            <div className="h-14 bg-yellow-200 text-center">
+              content halaman
+            </div>
+            <div className="h-14 bg-yellow-200 text-center">
+              content halaman
+            </div>
+            <div className="h-14 bg-yellow-200 text-center">
+              content halaman
+            </div>
+            <div className="h-14 bg-yellow-200 text-center">
+              content halaman
+            </div>
+            <div className="h-14 bg-yellow-200 text-center">TERAKHIR</div>
+          </div>
+        </div>
+      </div>
+      <footer className="block md:hidden h-14 bottom-0 left-0 right-0 bg-red-500  text-center">
         <h1>footer</h1>
       </footer>
     </div>
