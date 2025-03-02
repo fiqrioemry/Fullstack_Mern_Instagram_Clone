9549186972b1f9e0820559cee1f5ef43e6efa9d2
fiqrioemry <foemry@gmail.com>
2025-03-02 11:44:38 +0700

    update changes

--- client/src/components/messages/chat-container/ChatInput.jsx
+++ client/src/components/messages/chat-container/ChatInput.jsx
@@ -1,32 +1,55 @@
 import { Send, X } from "lucide-react";
+import { useEffect, useState } from "react";
 import { chatControl, chatState } from "@/config";
 import { useChatStore } from "@/store/useChatStore";
 import { useFormSchema } from "@/hooks/useFormSchema";
-import AttachComponent from "../../form/AttachComponent";
+import AttachComponent from "@/components/form/AttachComponent";
 
 const ChatInput = () => {
-  const { sendChat } = useChatStore();
-
+  const { sendChat, selectedUser } = useChatStore();
+  const [imagePreview, setImagePreview] = useState(null);
   const chatForm = useFormSchema(chatState, chatControl, sendChat);
+
+  useEffect(() => {
+    if (chatForm.values.image) {
+      const imageUrl = URL.createObjectURL(chatForm.values.image);
+      setImagePreview(imageUrl);
+
+      return () => {
+        URL.revokeObjectURL(imageUrl);
+      };
+    } else {
+      setImagePreview(null);
+    }
+  }, [chatForm.values.image]);
+
+  useEffect(() => {
+    chatForm.resetForm();
+  }, [selectedUser]);
+
   return (
     <div className="relative">
-      {chatForm.values.image && (
-        <div className="absolute bottom-14 p-2  w-40 h-40 overflow-hidden">
-          <div className="absolute -top-5 -right-5">
-            <X />
+      {/* Preview Gambar */}
+      {imagePreview && (
+        <div className="absolute left-2 bottom-20">
+          <button
+            className="absolute -top-2 -right-2 bg-white rounded-full p-1 shadow-md"
+            onClick={() => chatForm.setFieldValue("image", null)}
+            type="button"
+          >
+            <X className="w-4 h-4 text-muted-foreground" />
+          </button>
+          <div className="w-40 h-40 overflow-hidden animate-fadeIn">
+            <img
+              src={imagePreview}
+              className="w-full h-full object-cover rounded-lg"
+              alt="preview"
+            />
           </div>
-          <img
-            src={URL.createObjectURL(chatForm.values.image)}
-            className="w-full h-full object-cover "
-            onLoad={(e) => {
-              if (e.target.src.startsWith("blob:")) {
-                URL.revokeObjectURL(e.target.src);
-              }
-            }}
-            alt="image"
-          />
         </div>
       )}
+
+      {/* Form Input */}
       <form
         onSubmit={chatForm.handleSubmit}
         className="border-t border-muted md:p-4 p-2 flex items-center space-x-4"
@@ -42,8 +65,8 @@ const ChatInput = () => {
         <AttachComponent form={chatForm} name="image" />
         <button
           type="submit"
-          disabled={!chatForm.dirty}
-          className="p-1 btn-accent disabled:cursor-not-allowed"
+          disabled={!chatForm.values.message.trim() && !chatForm.values.image}
+          className="p-1 btn-accent disabled:cursor-not-allowed disabled:opacity-50"
         >
           <Send className="h-5 w-5" />
         </button>
