b1b6ea4f0ef2593431ba58b8e7e88639227845a3
fiqrioemry <foemry@gmail.com>
2025-03-05 10:33:25 +0700

    update changes

--- client/src/components/post/PostInput.jsx
+++ client/src/components/post/PostInput.jsx
@@ -1,6 +1,5 @@
 /* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react/prop-types */
-
 import { useEffect, useRef } from "react";
 import { useFormSchema } from "@/hooks/useFormSchema";
 import { commentControl, commentState } from "@/config";
